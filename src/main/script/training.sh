f23dcd54a1c3b6b2779ffce5777d7d9137b8ea05
jorge vea-murguia <jveamurguia@itainnova.es>
2022-05-24 22:33:31 +0200

    cambio de parametros en compose file

--- src/main/script/training.sh
+++ src/main/script/training.sh
@@ -2,4 +2,8 @@
 
 docker run -it --name training --network=chatbot_chatbot-network  -e PYTHONPATH=./ rasa:1.0 /bin/bash -c 'python3 scripts/generate_files.py;rasa train --data data/training/nlu -c config.yml -d data/training/domain.yml --num-threads 50 --out /python/administrator/models/GDA/AOD/ --fixed-model-name model-chatbot'
 docker commit training rasa:1.1
-docker-compose -f ./src/main/docker/docker-compose.yml --project-name chatbot --project-directory --env-file ./src/main/docker/.rasa1.1 . up -d
+export VERSION_RASA_MODEL=1.1
+
+echo " cambiar en el fichero de entorno .env la version de rasa 1.0 por 1.1  "
+
+docker-compose -f ./src/main/docker/docker-compose.yml --project-name chatbot --project-directory . up -d
